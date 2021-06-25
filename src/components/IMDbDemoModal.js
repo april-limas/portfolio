fd947d825606882c065198a1f46fb097719022c1
April Limas <aprilj.limas@gmail.com>
2021-06-25 18:40:49 -0400

    modified projects card details' sizes

--- src/components/IMDbDemoModal.js
+++ src/components/IMDbDemoModal.js
@@ -10,7 +10,7 @@ export const IMDbDemoModal = ({closeModal}) => {
                     <h2>Reel Reviews Demo</h2>
                 </div>
                 <div className="media-container">
-                    <iframe src="https://www.youtube.com/embed/Q8xSoqvRykU?modestbranding=1&rel=0&showinfo=0&loop=1&autoplay=1&mute=1" className="demo-video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/Q8xSoqvRykU?modestbranding=1&rel=0&showinfo=0&loop=1&autoplay=1&mute=1" title="Reel Reviews Demo" className="demo-video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
